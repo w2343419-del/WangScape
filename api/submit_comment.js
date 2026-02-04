659aa2b42f952dcb51cb99ea90bdfc16fcc21557
w2343419@gmail.com <w2343419@gmail.com>
2026-02-04 11:21:29 +0800

    Fix CommonJS export and simplify Vercel config

--- api/submit_comment.js
+++ api/submit_comment.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const origin = req.headers.origin || "*";
   res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
