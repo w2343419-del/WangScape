5dc76f7119a46b8fd111540020edbd06b0ef35e2
w2343419@gmail.com <w2343419@gmail.com>
2026-02-15 15:01:22 +0800

    feat: 删除WSwriter启动脚本，简化项目结构

--- run-wswriter.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-REM 从.env文件加载环境变量
-for /f "tokens=1,2 delims==" %%a in ('type .env ^| findstr /v "^#" ^| findstr /v "^$"') do (
-    set "%%a=%%b"
-)
-REM 启动WSwriter
-WSwriter.exe
-pause
