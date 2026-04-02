abf9684fe4cc807401194d72ae0b88748e5af6b7
w2343419@gmail.com <w2343419@gmail.com>
2026-04-02 17:22:42 +0800

    Remove Go toolchain from git tracking

--- .tools/go/src/runtime/testdata/testprog/cpuflags_amd64.s
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-#include "textflag.h"
-
-TEXT	·checkAVX(SB), NOSPLIT|NOFRAME, $0-0
-	VXORPS	X1, X2, X3
-	RET
